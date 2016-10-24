f0193dd732a33940369f982977d1861c4d8c29c9
cteeps922 <cpteeple@mail.roanoke.edu>
2016-10-24 09:23:34 -0400

    Add files via upload

---
+++ homepageTabs.js
@@ -0,0 +1,13 @@
+var myTabs = document.getElementsByClassName("SideBarTab");
+var myContents = document.getElementsByClassName("ContentPage");
+for (i = 0; i < myTabs.length; i++) {
+    myTabs[i].addEventListener("click", function(){
+	
+	for (j = 0; j < myContents.length; j++) {
+	    myContents[j].setAttribute("class", "isHidden")
+	}
+//	myContents[i].removeClass("isHidden");
+    })
+}
+			      
+
