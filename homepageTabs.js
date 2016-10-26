352c06ce6c239fb5543ae8facbad87a943db5b51
cteeps922 <cpteeple@mail.roanoke.edu>
2016-10-26 16:05:08 -0400

    Add files via upload
    The tabs have handlers that show the one that is clicked on and hide all others

--- homepageTabs.js
+++ homepageTabs.js
@@ -1,12 +1,21 @@
 var myTabs = document.getElementsByClassName("SideBarTab");
-var myContents = document.getElementsByClassName("ContentPage");
+
 for (i = 0; i < myTabs.length; i++) {
     myTabs[i].addEventListener("click", function(){
-	
-	for (j = 0; j < myContents.length; j++) {
-	    myContents[j].setAttribute("class", "isHidden")
-	}
-//	myContents[i].removeClass("isHidden");
+	var myContents = document.getElementsByClassName("ContentPage");
+	var news = document.getElementById("NewsContent");
+	var phil = document.getElementById("PhilanthropyContent");
+	var history = document.getElementById("HistoryContent");
+	var database = document.getElementById("DataBaseContent");
+	var contentToChange = String(this.getAttribute("data-content"));
+	var contentToAddClass = document.getElementById(contentToChange);
+
+	news.className = "isHidden";
+	phil.className = "isHidden";
+	history.className = "isHidden";
+	database.className = "isHidden";
+
+	contentToAddClass.className = "ContentPage";
     })
 }
 			      
